23ad1aee883fa9722dc45650e9759dfebe9648f1
Sayantak <sayantakkarar@gmail.com>
2019-09-13 17:57:59 +0530

    Added colors

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void soundPlay(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Widget buildKey(int soundNumber, String name, Color c) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          soundPlay(soundNumber);
+        },
+        child: Text(
+          name,
+          style: TextStyle(fontFamily: 'Pacifico'),
+        ),
+        color: c,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(1, 'Do', Colors.red),
+              buildKey(2, 'Re', Colors.orange),
+              buildKey(3, 'Mi', Colors.yellow),
+              buildKey(4, 'Fa', Colors.green),
+              buildKey(5, 'So', Colors.teal),
+              buildKey(6, 'La', Colors.blue),
+              buildKey(7, 'Ti', Colors.purple),
+            ],
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
